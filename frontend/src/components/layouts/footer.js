e955f44e66115479a3eead23150b6477e0a7140e
Ankit Raj <ankitanand803119@gmail.com>
2023-07-16 22:53:46 +0530

    first commit

---
+++ frontend/src/components/layouts/footer.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+
+const footer = () => {
+  return (
+    <Fragment>
+      <footer className="">
+        <p className="">
+         <span className="footer_text">
+
+          Sai Enterprises 	&#169; - 2022-2023, All Rights Reserved
+         </span>
+        </p>
+      </footer>
+    </Fragment>
+  );
+};
+
+
+
+
+export default footer;
